76185d9f17a80576004452b06a28cac33de74cfd
Kats676 <jastinbobik83@gmail.com>
2026-07-13 18:30:56 +0400

    Update FreeCamMixin.java

--- src/main/java/com/modgg/client/mixin/FreeCamMixin.java
+++ src/main/java/com/modgg/client/mixin/FreeCamMixin.java
@@ -1,20 +1,24 @@
 package com.modgg.client.mixin;
 
 import com.modgg.client.ModGGClient;
-import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(BackgroundRenderer.class)
-public class LavaCamMixin {
+@Mixin(Camera.class)
+public class FreeCamMixin {
 
-    @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
-    private static void onApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
-        if (ModGGClient.lavaCam.enabled && camera.getSubmersionType() == net.minecraft.block.enums.CameraSubmersionType.LAVA) {
-            ci.cancel();
+    @Inject(method = "update", at = @At("TAIL"))
+    private void onCameraUpdate(net.minecraft.world.BlockView area, net.minecraft.entity.Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci) {
+        if (ModGGClient.freeCam.enabled) {
+            Camera camera = (Camera) (Object) this;
+            ((CameraAccessor) camera).setPos(new net.minecraft.util.math.Vec3d(
+                ModGGClient.freeCam.fakeX, 
+                ModGGClient.freeCam.fakeY, 
+                ModGGClient.freeCam.fakeZ
+            ));
         }
     }
 }
