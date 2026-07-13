6866814bae860212227137ac9d11378be6393cf6
Kats676 <jastinbobik83@gmail.com>
2026-07-13 18:24:15 +0400

    Create CameraAccessor.java

---
+++ src/main/java/com/modgg/client/mixin/CameraAccessor.java
@@ -0,0 +1,12 @@
+package com.modgg.client.mixin;
+
+import net.minecraft.client.render.Camera;
+import net.minecraft.util.math.Vec3d;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(Camera.class)
+public interface CameraAccessor {
+    @Accessor("pos")
+    void setPos(Vec3d pos);
+}
