b19070fcfc518db0ab7f12da59fd9e4cb0d1d1bc
Kats676 <jastinbobik83@gmail.com>
2026-07-13 18:25:49 +0400

    Update FreeCamMixin.java

--- src/main/java/com/modgg/client/mixin/FreeCamMixin.java
+++ src/main/java/com/modgg/client/mixin/FreeCamMixin.java
@@ -1,27 +1,20 @@
 package com.modgg.client.mixin;
 
 import com.modgg.client.ModGGClient;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.BackgroundRenderer;
 import net.minecraft.client.render.Camera;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Camera.class)
-public class FreeCamMixin {
+@Mixin(BackgroundRenderer.class)
+public class LavaCamMixin {
 
-    @Inject(method = "update", at = @At("TAIL"))
-    private void onCameraUpdate(net.minecraft.world.BlockView area, net.minecraft.entity.Entity focusedEntity, boolean thirdPerson, boolean inverseView, float tickDelta, CallbackInfo ci) {
-        // Если фрикам включен, принудительно двигаем камеру по кастомным координатам летящего "духа"
-        if (ModGGClient.freeCam.enabled) {
-            Camera camera = (Camera) (Object) this;
-            // Устанавливаем положение камеры в пространстве отдельно от игрока
-            ((CameraAccessor) camera).setPos(new net.minecraft.util.math.Vec3d(
-                ModGGClient.freeCam.fakeX, 
-                ModGGClient.freeCam.fakeY, 
-                ModGGClient.freeCam.fakeZ
-            ));
+    @Inject(method = "applyFog", at = @At("HEAD"), cancellable = true)
+    private static void onApplyFog(Camera camera, BackgroundRenderer.FogType fogType, float viewDistance, boolean thickFog, float tickDelta, CallbackInfo ci) {
+        if (ModGGClient.lavaCam.enabled && camera.getSubmersionType() == net.minecraft.block.enums.CameraSubmersionType.LAVA) {
+            ci.cancel();
         }
     }
 }
