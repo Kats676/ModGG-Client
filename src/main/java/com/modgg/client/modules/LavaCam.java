455e7c016efd7a290ddb4cb30551cb29ce5bc3d0
Kats676 <jastinbobik83@gmail.com>
2026-07-13 15:19:43 +0400

    Create LavaCam.java

---
+++ src/main/java/com/modgg/client/modules/LavaCam.java
@@ -0,0 +1,9 @@
+package com.modgg.client.modules;
+
+/**
+ * kats676 — Зрение в лаве
+ */
+public class LavaCam {
+    public static boolean enabled = false;
+    public void toggle() { enabled = !enabled; }
+}
